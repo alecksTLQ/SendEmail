8933a51ac9aa69c859be46c973cc24e7e7ff5e05
Alex Garcia <mgarciag22@miumg.edu.gt>
2021-03-25 00:31:58 -0600

    envio de correo por visual basic

---
+++ sendEmail/Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEnviar = New System.Windows.Forms.Button()
+        Me.txtAsunto = New System.Windows.Forms.TextBox()
+        Me.txtMensaje = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnEnviar
+        '
+        Me.btnEnviar.Location = New System.Drawing.Point(180, 184)
+        Me.btnEnviar.Name = "btnEnviar"
+        Me.btnEnviar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnviar.TabIndex = 0
+        Me.btnEnviar.Text = "Enviar"
+        Me.btnEnviar.UseVisualStyleBackColor = True
+        '
+        'txtAsunto
+        '
+        Me.txtAsunto.Location = New System.Drawing.Point(49, 35)
+        Me.txtAsunto.Name = "txtAsunto"
+        Me.txtAsunto.Size = New System.Drawing.Size(206, 20)
+        Me.txtAsunto.TabIndex = 1
+        '
+        'txtMensaje
+        '
+        Me.txtMensaje.Location = New System.Drawing.Point(49, 88)
+        Me.txtMensaje.Name = "txtMensaje"
+        Me.txtMensaje.Size = New System.Drawing.Size(206, 20)
+        Me.txtMensaje.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(327, 282)
+        Me.Controls.Add(Me.txtMensaje)
+        Me.Controls.Add(Me.txtAsunto)
+        Me.Controls.Add(Me.btnEnviar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnEnviar As System.Windows.Forms.Button
+    Friend WithEvents txtAsunto As System.Windows.Forms.TextBox
+    Friend WithEvents txtMensaje As System.Windows.Forms.TextBox
+
+End Class
---
+++ sendEmail/Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Net.Mail
+
+Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnEnviar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEnviar.Click
+        Dim smtp_server As New SmtpClient
+        Dim e_mail As New MailMessage
+        smtp_server.UseDefaultCredentials = False
+        smtp_server.Credentials = New Net.NetworkCredential("alecksgarciazr16@gmail.com", "kris-2021#")
+        smtp_server.Port = 25
+        smtp_server.EnableSsl = True
+        smtp_server.Host = "smtp.gmail.com"
+
+        e_mail = New MailMessage
+        e_mail.From = New MailAddress("alecksgarciazr16@gmail.com")
+        e_mail.To.Add("mgarciag22@miumg.edu.gt")
+        e_mail.Subject = txtAsunto.Text
+        e_mail.IsBodyHtml = False
+        e_mail.Body = txtMensaje.Text
+        smtp_server.Send(e_mail)
+
+
+    End Sub
+End Class
